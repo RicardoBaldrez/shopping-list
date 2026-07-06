085f8333745ab16bce2998df076f4cb51b4e20a5
Ricardo <ricardo.baldrez@gmail.com>
2026-07-06 18:28:44 -0300

    feat: add button props

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,6 +1,7 @@
 import { View, Image } from 'react-native';
 
 import { styles } from './styles';
+import { Button } from '@/components/Button';
 
 export function Home() {
   return (
@@ -9,6 +10,7 @@ export function Home() {
         style={styles.logo}
         source={require("@/assets/logo.png")}
       />
+      <Button title="Adicionar" />
     </View>
   );
 }
